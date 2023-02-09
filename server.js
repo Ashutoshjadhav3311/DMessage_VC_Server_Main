fc1d20bc82331803e4637ec8c41f60094dbaeaed
Ashutosh Jadhav <104265231+Ashutoshjadhav3311@users.noreply.github.com>
2023-02-09 15:28:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = require("socket.io")(server, {
 
   },
 });
-app.use(function(req, res, next) {
+io.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
