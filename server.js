fae4a77577df690857be5c93e6bd9dde4930109a
Ashutosh Jadhav <104265231+Ashutoshjadhav3311@users.noreply.github.com>
2023-02-09 15:17:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const io = require("socket.io")(server, {
     origin: "http://localhost:3000", //for cloud https://dmessagevc.onrender.com/ for local http://localhost:3000
     methods: ["GET", "POST"],
     allowedHeaders:"*",
-  Access-Control-Allow-Origin: *
+ app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
   },
 });
 
